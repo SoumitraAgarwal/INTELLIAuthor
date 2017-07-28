c58f1502ca3f09ad600212caece336743a976ee0
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-07-28 23:20:33 +0530

    Minor corrections

--- Model/__main__.py
+++ Model/__main__.py
@@ -22,4 +22,4 @@ mapRhymes  		= process.get_rhymes(Lasts)
 millis 			= process.log("Created rhyme maps for the document in ", millis, 1)
 millis 			= process.log("Now I will start writing!\n", millis, 0)
 
-author.write(Firsts, Centres, Lasts, mapFirsts, mapCentres, mapLasts, mapRhymes, 10, 3)
+author.write_poem(Firsts, Centres, Lasts, mapFirsts, mapCentres, mapLasts, mapRhymes, 10, 7)
